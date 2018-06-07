3f6766de08060ff009de9f55ae97b63f93148f7e
Kohei Hiraga <white.line.oar@gmail.com>
2018-06-08 01:37:39 +0900

    Use system-installed msgpack

--- include/rpc/msgpack.hpp
+++ include/rpc/msgpack.hpp
@@ -20,13 +20,4 @@
 #pragma clang system_header
 #endif
 
-#include "rpc/msgpack/object.hpp"
-#include "rpc/msgpack/iterator.hpp"
-#include "rpc/msgpack/zone.hpp"
-#include "rpc/msgpack/pack.hpp"
-#include "rpc/msgpack/unpack.hpp"
-#include "rpc/msgpack/sbuffer.hpp"
-#include "rpc/msgpack/vrefbuffer.hpp"
-#include "rpc/msgpack/version.hpp"
-#include "rpc/msgpack/type.hpp"
-
+#include <msgpack.hpp>
